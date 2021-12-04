1f49053d385a1a743522a8dab5f2ef2aa09ffb9c
felkamp <felkamp@mail.ru>
2021-12-04 19:22:13 +0300

    add ELK

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
 import faust
+import sentry_sdk
+
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from starlette.middleware import Middleware
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from src.api.routes import api_router
 from src.core.config import get_settings
 
 settings = get_settings()
 
+sentry_sdk.init(
+    dsn=settings.SENTRY_DNS,
+    environment=settings.SENTRY_ENVIRONMENT
+    )
+
+middleware = [
+    Middleware(SentryAsgiMiddleware),
+]
+
 app = FastAPI(
     version="1.0.0",
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
+    middleware=[Middleware(SentryAsgiMiddleware)]
 )
 
 app.router.include_router(api_router, prefix=settings.API_PREFIX)
